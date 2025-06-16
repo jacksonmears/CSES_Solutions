a102e0362cae812a33d8cdc767160e3f53afe928
Jackson Mears <jackson.mears2002@gmail.com>
2025-06-16 12:42:50 -0500

    commit #59
    added Tree-Diameter.cpp

---
+++ Tree-Algorithms/Tree-Diameter.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+constexpr ll MAX = 9e18;
+constexpr ll MOD = 1e9 + 7;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define REP(i,a,b) for (ll i = a; i <= b; i++)
+
+ll n;
+vector<vl> adjacencyMatrix;
+ll maxDistance = 0;
+
+
+//using bfs to find diameter of tree
+ll findFurthestNode(ll node) {
+
+    queue<pl> q; q.emplace(node, 0);
+
+    vector<bool> seen(n+1, false); seen[node] = true;
+
+    ll currentNode;
+    while (!q.empty()) {
+        currentNode = q.front().first;
+        ll currentDistance = q.front().second;
+        q.pop();
+
+        for (auto child : adjacencyMatrix[currentNode]) {
+            if (!seen[child]) {
+                maxDistance = max(maxDistance, currentDistance+1);
+                q.emplace(child, currentDistance+1);
+                seen[child] = true;
+            }
+        }
+    }
+    //return the last node
+    return currentNode;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    cin >> n;
+    adjacencyMatrix = vector<vl> (n+1);
+    REP(i, 0, n-2) {
+        ll a, b; cin >> a >> b;
+        adjacencyMatrix[a].pb(b); adjacencyMatrix[b].pb(a);
+    }
+
+    ll furthestNodeFromOne = findFurthestNode(1);
+    ll furthestNodeFromEnd = findFurthestNode(furthestNodeFromOne);
+
+
+    cout << maxDistance;
+
+
+
+
+
+    return 0;
+}
