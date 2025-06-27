7aba8b852d025717858477afe221807f9531c43d
Jackson Mears <jackson.mears2002@gmail.com>
2025-06-27 12:25:25 -0500

    commit #71
    added Sliding-Window-Minimum.cpp

---
+++ Sliding-Window-Problems/Sliding-Window-Minimum.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+constexpr ll MAX = 9e18;
+constexpr ll MOD = 1e9 + 7;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define REP(i,a,b) for (ll i = a; i <= b; i++)
+
+deque<ll> minimumStack;
+vl inputArray;
+
+void updateStack(const ll i) {
+    while (!minimumStack.empty() && inputArray[minimumStack.back()] >= inputArray[i]) {
+        minimumStack.pop_back();
+    }
+    minimumStack.push_back(i);
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    ll k, n, x, a, b, c; cin >> k >> n >> x >> a >> b >> c;
+    inputArray = vl(k); inputArray[0]=x;
+    REP(i, 1, k-1) inputArray[i] = (inputArray[i-1]*a + b) % c;
+
+
+    REP(i, 0, n-1) updateStack(i);
+    ll xorMinimum = inputArray[minimumStack.front()];
+
+    REP(i, 0, k-n-1) {
+        if (!minimumStack.empty() && minimumStack.front() <= i) minimumStack.pop_front();
+        updateStack(i+n);
+        xorMinimum ^= inputArray[minimumStack.front()];
+    }
+
+    cout << xorMinimum;
+
+    return 0;
+}
