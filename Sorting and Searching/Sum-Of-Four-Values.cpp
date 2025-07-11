910a04bbabbfa02b7cc4f1f78f158fa963610bfc
Jackson Mears <jackson.mears2002@gmail.com>
2025-07-11 17:45:16 -0500

    added sum of four values!

--- Sorting and Searching/Sum-Of-Four-Values.cpp
+++ Sorting and Searching/Sum-Of-Four-Values.cpp
@@ -21,27 +21,31 @@ int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
 
     ll n, target; cin >> n >> target;
-    vector<pl> elements(n);
-    rep(i, 0, n-1) {
-        ll x; cin >> x;
-        elements[i] = {x, i+1};
-    }
-    ranges::sort(elements);
+    vector<ll> elements(n);
+    rep(i, 0, n-1) cin >> elements[i];
 
+    vector<pair<ll, pl>> pair_sums;
     rep(i, 0, n-1) {
-        if (i > 0 && elements[i].first == elements[i-1].first) continue;
         rep(j, i+1, n-1) {
-            if (j > i+1 && elements[j].first == elements[j-1].first) continue;
-            rep(k, j+1, n-1) {
-                ll need = target - elements[i].first - elements[j].first - elements[k].first;
-                auto it = lower_bound(elements.begin()+k+1, elements.end(), mp(need, 0ll));
-                if (it->second <= n && it->first==need) return cout << elements[i].second << " " << elements[j].second << " " << elements[k].second << " " << it->second, 0;
-            }
+            pair_sums.push_back({elements[i] + elements[j], {i, j}});
         }
     }
 
+    unordered_map<ll, vector<pl>> seen;
+    for (auto &[sum, p1] : pair_sums) {
+        ll need = target - sum;
+        if (seen.count(need)) {
+            for (auto &[i2, j2] : seen[need]) {
+                if (p1.first != i2 && p1.first != j2 && p1.second != i2 && p1.second != j2) {
+                    return cout << p1.first+1 << " " << p1.second+1 << " " << i2+1 << " " << j2+1, 0;
+                }
+            }
+        }
+        seen[sum].push_back(p1);
+    }
 
-    cout << "IMPOSSIBLE";
-
+    cout << "IMPOSSIBLE\n";
     return 0;
 }
+
+
