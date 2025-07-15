1ade4e07d88d8fde8a519c01f3202a111877be28
Jackson Mears <jackson.mears2002@gmail.com>
2025-07-14 22:03:10 -0500

    changed cmake file among some other minor fixes

---
+++ Dynamic-Programming/Array-Description.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+constexpr ll MAX = 9e18;
+constexpr ll MOD = 1e9 + 7;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (ll i = a; i <= b; i++)
+
+
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+
+    ll n, m; cin >> n >> m;
+    vl elements; rep(i, 0, n-1) cin >> elements[i];
+
+
+
+
+
+    return 0;
+}
+
+
