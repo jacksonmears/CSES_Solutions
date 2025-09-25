10d1d1cc64ae66ff8b1a17a880ea6ca907ffc22b
jacksonmears <jackson.mears2002@gmail.com>
2025-09-25 18:50:19 -0500

    adde xor pyramid peak

---
+++ Bitwise-Operations/Xor-Pyramid-Peak.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef vector<pl> vpl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+constexpr int MOD = 1e9 + 7;
+ 
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = a; i <= b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+ 
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    int n; cin >> n;
+    vl elements(n);
+    
+    rep(i, 0, n-1)
+        cin >> elements[i];
+
+    ll ans = 0;
+    int m = n - 1;
+    rep(i, 0, n-1) {
+        // include a[i] iff (i) (which is i-1 in 1-based -> we use zero-based)
+        // satisfies (i & m) == i
+        if ((i & m) == i) ans ^= elements[i];
+    }
+
+    cout << ans;
+    
+    return 0;
+}
