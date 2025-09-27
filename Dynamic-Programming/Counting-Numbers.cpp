f611360bba5cfde5a244f050de7544d47fd63acd
jacksonmears <jackson.mears2002@gmail.com>
2025-09-27 11:42:55 -0500

    added counting numbers

--- Dynamic-Programming/Counting-Numbers.cpp
+++ Dynamic-Programming/Counting-Numbers.cpp
@@ -1,73 +1,77 @@
+#pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
 typedef vector<ll> vl;
 typedef pair<ll,ll> pl;
-typedef vector<bool> vb;
-typedef vector<pl> vp;
+typedef vector<pl> vpl;
 typedef vector<vl> vvl;
-constexpr ll MAX = 9e18;
-constexpr ll MOD = 1e9 + 7;
-
+typedef vector<bool> vb;
+constexpr int MOD = 1e9 + 7;
+ 
 #define f first
 #define s second
 #define pb push_back
 #define mp make_pair
-#define rep(i,a,b) for (ll i = a; i <= b; i++)
-#define repr(i, a, b) for (ll i = a; i >= b; i--)
+#define rep(i,a,b) for (int i = a; i <= b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+ 
+ 
+ 
+ll a, b, dp[20][11][2][2];
+vi digits;
 
-
-const ll MAXN = 20;
-
-bool tight[MAXN];
-ll dp[10][MAXN];
-
-ll solve(ll value){
-    if(value <= 10) return value;
-
-    vl digits;
-    memset(dp, 0, sizeof(dp));
-    memset(tight, 0, sizeof(tight));
-
-    while (value) {
-        digits.push_back(value%10);
-        value /= 10;
+// prev_idx: 0..9 are digits, 10 means "no previous digit yet" (leading zeros so far)
+ll dfs(int pos, int prev_idx, int tight, int lead) {
+    if (pos == (int)digits.size()) {
+        // we've constructed one number (including 0 when all digits were leading zeros)
+        return 1;
     }
-    ranges::reverse(digits);
-    const ll N = (ll)digits.size();
+    ll &memo = dp[pos][prev_idx][tight][lead];
+    if (memo != -1) return memo;
 
-    tight[0] = true;
-    rep(i, 1, N-1)
-        tight[i] = tight[i-1] && (digits[i] != digits[i-1]);
-
-    rep(digit, 1, digits[0])
-        dp[digit][0] = 1;
-
-    rep(i, 1, N-1) {
-        if(tight[i-1])
-            rep(digit, 0, digits[i])
-                if(digit != digits[i-1])
-                    dp[digit][i]++;
-
-        rep(digit, 1, 9)
-            dp[digit][i]++;
-
-        rep(digit1, 0, 9)
-            rep(digit2, 0, 9)
-                if(digit1 != digit2)
-                    dp[digit2][i] += dp[digit1][i-1];
+    memo = 0;
+    int limit = tight ? digits[pos] : 9;
+    for (int d = 0; d <= limit; ++d) {
+        int next_lead = lead && (d == 0);
+        // If not leading zeros and digit equals previous => invalid
+        if (!next_lead && !lead && d == prev_idx) continue;
+        // Compute next prev index:
+        int next_prev = next_lead ? 10 : d;
+        int next_tight = tight && (d == limit);
+        memo += dfs(pos + 1, next_prev, next_tight, next_lead);
     }
-
-    ll count = tight[N-1];
-    rep(digit, 0, 9)
-        count += dp[digit][N-1];
-    return count;
+    return memo;
 }
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(nullptr);
+ll count_up_to(ll x) {
+    if (x < 0) return 0;
+    if (x == 0) return 1; // only number 0
 
-    ll a, b; cin >> a >> b;
-    cout << solve(b) - solve(a-1);
+    digits.clear();
+    while (x > 0) {
+        digits.push_back((int)(x % 10));
+        x /= 10;
+    }
+    reverse(digits.begin(), digits.end());
+    // initialize dp with -1
+    memset(dp, -1, sizeof(dp));
+    // start at pos 0, prev = 10 (no prev), tight = 1, lead = 1 (we haven't placed non-zero yet)
+    return dfs(0, 10, 1, 1);
 }
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> a >> b;
+
+    cout << count_up_to(b) - count_up_to(a - 1);
+
+    return 0;
+}
